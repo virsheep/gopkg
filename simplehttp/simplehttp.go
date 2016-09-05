977b0103839ed260610e39676c7e59b722701bc7
sheep <whithey870106@gmail.com>
2016-09-05 20:06:46 +0800

    yangxu update simplehttp.Post

--- simplehttp/simplehttp.go
+++ simplehttp/simplehttp.go
@@ -4,35 +4,72 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func Test() {
 	fmt.Println("Test : ok")
 }
 
-func Get(httpUrl string, httpParams string) (respBody string, respHttpcode int, err error) {
+func Get(httpUrl string, httpParams string) (respHttpcode int, respBody string, err error) {
 
-	finalUrl := httpUrl + "?" + httpParams
+	finalUrl := httpUrl
+	if httpParams != "" {
+		finalUrl = httpUrl + "?" + httpParams
+	}
 
 	req, err := http.NewRequest("GET", finalUrl, nil)
+
 	if err != nil {
 		return
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
+
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
 	if err != nil {
 		return
 	}
 
 	respHttpcode = resp.StatusCode
+	respBody = string(body)
 
-	body, err := ioutil.ReadAll(resp.Body)
+	return
+
+}
+
+func Post(httpUrl string, httpData string) (respHttpcode int, respBody string, err error) {
+
+	finalUrl := httpUrl
+
+	req, err := http.NewRequest("POST", finalUrl, strings.NewReader(httpData))
+
 	if err != nil {
 		return
 	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 
+	if err != nil {
+		return
+	}
+
+	respHttpcode = resp.StatusCode
 	respBody = string(body)
 
 	return
