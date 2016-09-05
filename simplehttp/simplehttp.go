c30bd76acdc2e206fcb5cec82190245eff1a500a
sheep <whithey870106@gmail.com>
2016-09-05 18:08:48 +0800

    yangxu update

--- simplehttp/simplehttp.go
+++ simplehttp/simplehttp.go
@@ -1,7 +1,40 @@
 package simplehttp
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func Test() {
-	fmt.Println("vim-go")
+	fmt.Println("Test : ok")
+}
+
+func Get(httpUrl string, httpParams string) (respBody string, respHttpcode int, err error) {
+
+	finalUrl := httpUrl + "?" + httpParams
+
+	req, err := http.NewRequest("GET", finalUrl, nil)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	respHttpcode := resp.StatusCode
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody := string(body)
+
+	return
+
 }
